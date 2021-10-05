a30224e89209112307268109a6d6e5ee5bb344c9
Santhosh Balu <chenkarai@gmail.com>
2021-10-05 15:16:12 +1100

    Add formik, sample form, save to table

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { generate } from "shortid";
+import { MyForm } from "./MyForm";
+import { MyTable } from "./MyTable";
 
 const App = () => {
+  const [rows, setRows] = useState([
+    {
+      id: "45",
+      firstName: "bob",
+      lastName: "bob2",
+      email: "bob@bob.com"
+    }
+  ]);
 
   return (
-    <h1>
-      react-formik-material-table
-    </h1>
+    <div style={{ textAlign: "center" }}>
+      <MyForm
+        onSubmit={data => {
+          setRows(currentRows => [
+            {
+              id: generate(),
+              ...data
+            },
+            ...currentRows
+          ]);
+        }}
+      />
+      <MyTable rows={rows} />
+    </div>
   );
 };
 
